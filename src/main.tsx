add58a327f50c2eb15728745c461ba90d84331d7
Jay <jayjasani.softcolon@gmail.com>
2023-10-18 11:39:54 +0530

    persist gate

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider as StoreProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { PersistGate } from "redux-persist/integration/react";
+
 import App from './app';
 import { store, persistor } from './store';
 import addAuthTokenInterceptor from './lib/addAuthTokenInterceptor';
@@ -18,7 +19,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <StoreProvider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <PersistGate loading={null} persistor={persistor}>
     <HelmetProvider>
       <BrowserRouter>
         <Suspense>
@@ -28,6 +29,6 @@ root.render(
         </Suspense>
       </BrowserRouter>
     </HelmetProvider>
-    {/* </PersistGate> */}
+    </PersistGate>
   </StoreProvider>
 );
